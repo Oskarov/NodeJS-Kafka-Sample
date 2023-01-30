2d4a8c1e4ba6027fab276a439a0c51b60c8c43a3
Di <elven.legacy@hotmail.com>
2023-01-30 21:38:08 +0300

    Настройка консюмера

---
+++ consumer/index.js
@@ -0,0 +1,17 @@
+import Kafka from 'node-rdkafka';
+import eventType from '../eventType.js';
+
+let consumer = new Kafka.KafkaConsumer({
+  'group.id': 'kafka',
+  'metadata.broker.list': 'localhost:9092',
+}, {});
+
+consumer.connect();
+
+consumer.on('ready', () => {
+  console.log('consumer ready..')
+  consumer.subscribe(['test']);
+  consumer.consume();
+}).on('data', function(data) {
+  console.log(`received message: ${eventType.fromBuffer(data.value)}`);
+});
